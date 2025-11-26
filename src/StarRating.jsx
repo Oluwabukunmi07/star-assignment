a3f4afee4f62a6143dec11f3e3004cec8e9ec0e7
Oluwabukunmi07 <preciousbukunmi07@gmail.com>
2025-11-26 01:34:49 +0100

    changed

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -19,21 +19,6 @@ function StarRating({ rating }) {
     </div>
   );
 
-  /*
-    Here's the markup for a single star:
-
-    <img
-      alt=""
-      className="gold-star"
-      // src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
-      src="/star.svg"
-    />
-
-    Your job is to repeat this element
-    based on the `rating` prop.
-    If the rating is 4, we need 4 copies.
-  */
-
    
 }
 
